f62e682a786cd0741f55b25bb1e2922ddbcb7503
Tyby <tibofenyvesi84@gmail.com>
2018-07-02 21:59:31 +0100

    Counter is working

---
+++ src/store/reducer.js
@@ -0,0 +1,49 @@
+const initialState = {
+    counter: 0,
+	results: []
+}
+
+const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+	switch(action.type) {
+		case 'INCREMENT' : 
+			const newState = Object.assign({}, state);
+			newState.counter = state.counter +1;
+			return newState;
+			break;
+			
+		case 'DECREMENT' : 
+			return {
+				...state,
+				counter : state.counter -1
+			}
+			break;
+		case 'ADD' :
+			return {
+				...state,
+				counter: state.counter + action.value
+			}
+			break;
+		case 'SUB' :
+			return {
+				...state,
+				counter: state.counter - action.value
+			}
+			break;
+		case 'STORE_RESULT' :
+			return {
+				...state,
+				results: state.results.concat({id: new Date(), val:state.counter})
+			}
+			break;
+		default: 
+			return state;
+	}
+    
+};
+
+export default reducer;
